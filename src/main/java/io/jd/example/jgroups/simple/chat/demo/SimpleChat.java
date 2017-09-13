4447852ff59b3368b27462de922892ed6d65f53c
John Deng <john.deng@qq.com>
2017-09-13 09:54:23 +0800

    maintaining shared cluster state

--- src/main/java/io/jd/example/jgroups/simple/chat/demo/SimpleChat.java
+++ src/main/java/io/jd/example/jgroups/simple/chat/demo/SimpleChat.java
@@ -5,30 +5,32 @@ import org.jgroups.JChannel;
 import org.jgroups.Message;
 import org.jgroups.ReceiverAdapter;
 import org.jgroups.View;
+import org.jgroups.util.Util;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
+
+import static java.lang.System.*;
 
 @Slf4j
 public class SimpleChat extends ReceiverAdapter {
 
-    JChannel channel;
+    private JChannel channel;
 
-    String username = System.getProperty("user.name", "n/a");
+    private String username = getProperty("user.name", "n/a");
+
+    private final List<String> state = new LinkedList<String>();
 
 
     public void start() throws Exception {
 
-        channel = new JChannel(); // use the default config, udp.xml
-
+        channel=new JChannel();
         channel.setReceiver(this);
         channel.connect("ChatCluster");
-
+        channel.getState(null, 10000);
         eventLoop();
-
         channel.close();
-
-
     }
 
     private void eventLoop() {
@@ -39,8 +41,8 @@ public class SimpleChat extends ReceiverAdapter {
 
             try {
 
-                System.out.print("> ");
-                System.out.flush();
+                out.print("> ");
+                out.flush();
 
                 String line = in.readLine().toLowerCase();
 
@@ -63,14 +65,56 @@ public class SimpleChat extends ReceiverAdapter {
 
     public void viewAccepted(View newView) {
 
-        System.out.println("** view: " + newView);
+        out.println("** view: " + newView);
 
     }
 
 
     public void receive(Message msg) {
 
-        System.out.println(msg.getSrc() + ": " + msg.getObject());
+        String line=msg.getSrc() + ": " + msg.getObject();
+
+        out.println(line);
+
+        synchronized(state) {
+
+            state.add(line);
+
+        }
+
+    }
+
+    public void getState(OutputStream output) throws Exception {
+
+        synchronized(state) {
+
+            Util.objectToStream(state, new DataOutputStream(output));
+
+        }
+
+    }
+
+    public void setState(InputStream input) throws Exception {
+
+        List<String> list;
+
+        list= Util.objectFromStream(new DataInputStream(input));
+
+        synchronized(state) {
+
+            state.clear();
+
+            state.addAll(list);
+
+        }
+
+        out.println(list.size() + " messages in chat history):");
+
+        for(String str: list) {
+
+            out.println(str);
+
+        }
 
     }
 }
